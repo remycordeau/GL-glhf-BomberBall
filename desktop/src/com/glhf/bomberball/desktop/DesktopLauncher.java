10357d856e1c1811d98db6d535b49a3142a81c54
lagarde <jlagarde@enssat.fr>
2018-11-18 14:45:26 +0100

    changement de la taille de fenêtre

--- desktop/src/com/glhf/bomberball/desktop/DesktopLauncher.java
+++ desktop/src/com/glhf/bomberball/desktop/DesktopLauncher.java
@@ -5,8 +5,13 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.glhf.bomberball.Game;
 
 public class DesktopLauncher {
+	static final private int width = 4*200;
+	static final private int height = 3*200;
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = width;
+		config.height = height;
+		config.resizable = false;
 		new LwjglApplication(new Game(), config);
 	}
 }
