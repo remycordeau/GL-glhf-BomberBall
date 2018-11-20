bb859e5562b02f94b28285c42afb117db5e590cc
RomaneCastel <rcastel@enssat.fr>
2018-11-20 18:06:00 +0100

    Suppression classe Case Remontée des attributs position dans GameObject

--- core/src/com/glhf/bomberball/Character.java
+++ core/src/com/glhf/bomberball/Character.java
@@ -2,8 +2,6 @@ package com.glhf.bomberball;
 
 public abstract class Character extends GameObject {
     //attributes
-    protected int position_x;
-    protected int position_y;
     protected int life;
     protected int number_move_remaining;
     protected String sprite_path; // texture or string ?
--- core/src/com/glhf/bomberball/GameObject.java
+++ core/src/com/glhf/bomberball/GameObject.java
@@ -3,7 +3,10 @@ package com.glhf.bomberball;
 import com.badlogic.gdx.graphics.Texture;
 
 public abstract class GameObject {
-    private Texture appearance;
+    //attributes
+    protected int position_x;
+    protected int position_y;
+    protected Texture appearance;
     public Texture getTexture(){
         return appearance;
     }
