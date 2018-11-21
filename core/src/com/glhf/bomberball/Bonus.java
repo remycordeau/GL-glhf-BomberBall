80d9854795822002ea4d1d7f524122f775785e53
RomaneCastel <rcastel@enssat.fr>
2018-11-21 18:22:16 +0100

    ajout des classes speedboost, bombrangeboost, numberbombboost avec leur constructeur

--- core/src/com/glhf/bomberball/Bonus.java
+++ core/src/com/glhf/bomberball/Bonus.java
@@ -2,15 +2,6 @@ package com.glhf.bomberball;
 
 import com.badlogic.gdx.graphics.Texture;
 
-public class Bonus extends GameObject {
+public interface Bonus {
 
-    // constructor
-    public Bonus(int position_x, int position_y, Texture appearance) {
-        super(position_x, position_y, appearance);
-    }
-
-    @Override
-    public Texture getTexture() {
-        return null;
-    }
 }
