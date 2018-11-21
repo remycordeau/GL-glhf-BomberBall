baf7154aa2f201de591a147185567214af8cedea
RomaneCastel <rcastel@enssat.fr>
2018-11-21 17:50:57 +0100

    ajout methode dropBomb dans Player

--- core/src/com/glhf/bomberball/Player.java
+++ core/src/com/glhf/bomberball/Player.java
@@ -2,13 +2,22 @@ package com.glhf.bomberball;
 
 import com.badlogic.gdx.graphics.Texture;
 
+import java.util.Hashtable;
+
 public class Player extends Character {
     private int number_bomb_remaining;
+    private Hashtable<Bonus, int > bonus_owned;
 
     //constructor
-
     public Player(int position_x, int position_y, Texture appearance) {
         super(position_x, position_y, appearance);
         number_bomb_remaining=1;
+    }
+
+    public void dropBomb(int drop_position_x, int drop_position_y){
+        number_bomb_remaining-=1;
+        //if(Math.abs(position_x-drop_position_x)< bonus_owned.get(Object key_element)  and Math.abs(position_y-drop_position_y)<){
+
+        //}
     }
 }
