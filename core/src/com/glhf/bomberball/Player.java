d888fc0e2d037afa3049239cb6553ee0f41a4ee4
RomaneCastel <rcastel@enssat.fr>
2018-11-21 17:10:45 +0100

    ajout constructeyr Character

--- core/src/com/glhf/bomberball/Player.java
+++ core/src/com/glhf/bomberball/Player.java
@@ -7,11 +7,20 @@ public class Player extends Character {
 
     // constructor
     public Player(){ // temporary, create a file with parameter
-        position_x=0;
-        position_y=0;
+        super(position_x, position_y, appearance);
         life=1;
         number_bomb_remaining=1;
         number_move_remaining=5;
         //sprite_path
     }
+
+    // getters and setters
+    public int getNumberBombRemaining() {
+        return number_bomb_remaining;
+    }
+
+    public void setNumberBombRemaining(int number_bomb_remaining) {
+        this.number_bomb_remaining = number_bomb_remaining;
+    }
+
 }
