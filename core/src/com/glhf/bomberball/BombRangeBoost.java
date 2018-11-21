050e6e24d4cd4c1eba09819f20e417c099be43cb
RomaneCastel <rcastel@enssat.fr>
2018-11-21 23:04:25 +0100

    ajout contructeur sur les différents bonus

--- core/src/com/glhf/bomberball/BombRangeBoost.java
+++ core/src/com/glhf/bomberball/BombRangeBoost.java
@@ -1,4 +1,11 @@
 package com.glhf.bomberball;
 
 public class BombRangeBoost {
+    // attributes
+    private int bomb_range_boost;
+
+    // constructor
+    public BombRangeBoost() {
+        bomb_range_boost=0;
+    }
 }
--- core/src/com/glhf/bomberball/SpeedBoost.java
+++ core/src/com/glhf/bomberball/SpeedBoost.java
@@ -1,4 +1,11 @@
 package com.glhf.bomberball;
 
 public class SpeedBoost {
+    //attributes
+    private int speedBoost;
+
+    // constructor
+    public SpeedBoost() {
+        speedBoost=0;
+    }
 }
