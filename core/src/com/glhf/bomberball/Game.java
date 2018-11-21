09ac0bf16fc9d24faa83120fc8197d35660b74b6
thomas <tvoneuw@enssat.fr>
2018-11-21 18:24:23 +0100

    affichage image sans appui

--- core/src/com/glhf/bomberball/Game.java
+++ core/src/com/glhf/bomberball/Game.java
@@ -9,6 +9,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.Array;
 
 import java.io.File;
@@ -17,23 +18,29 @@ import java.util.HashMap;
 public class Game extends ApplicationAdapter {
 	private SpriteBatch batch;
 	private BitmapFont font;
+	private Texture test;
+	private TextureRegion region;
 	private HashMap<String, Texture> textures;
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		font = new BitmapFont();
+		test = new Texture(Gdx.files.internal("core/assets/badlogic.jpg"));
 		loadTextures();
+		region = new TextureRegion(test, 0, 0, 256, 256);
 		font.setColor(Color.RED);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(0, 0, 0, 0);
+		Gdx.gl.glClearColor(255, 255, 255, 255);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		font.draw(batch, "Natan il est trop beau", 200, 200);
+		batch.draw(region, 256, 256);
 		batch.end();
+
 	}
 	
 	@Override
@@ -57,7 +64,7 @@ public class Game extends ApplicationAdapter {
 				loadTextures(path + file.getName() + "/");
 				continue;
 			}
-			String stringId = file.getName().replace(".png", "");
+			String stringId = file.getName().replace(".jpg", "");
 			System.out.println(stringId);
 			textures.put(stringId, new Texture(Gdx.files.internal(path + file.getName())));
 		}
