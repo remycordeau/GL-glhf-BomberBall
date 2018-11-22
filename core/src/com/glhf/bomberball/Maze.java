f63b87ea35e4bf29b522ccddba3b55c0b6655975
lagarde <jlagarde@enssat.fr>
2018-11-22 17:27:38 +0100

    Avancement dans la lecture de fichiers labyrinthe en Json il manque encore l'importation de la Texture dans chaque GameObject.

--- core/src/com/glhf/bomberball/Maze.java
+++ core/src/com/glhf/bomberball/Maze.java
@@ -1,19 +1,13 @@
 package com.glhf.bomberball;
 
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.maps.tiled.TiledMap;
-import com.badlogic.gdx.maps.tiled.TmxMapLoader;
-import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.utils.Json;
-import com.google.gson.Gson;
+import com.google.gson.*;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.Reader;
+import java.lang.reflect.Type;
 
 public class Maze {
 
@@ -23,6 +17,7 @@ public class Maze {
     private Vector2[] positionStart;
     private Vector2 positionEnd;
     private GameObject[][] tab;
+    private static Gson gson;
 
     public Maze() {
         title = "Classic";
@@ -35,23 +30,67 @@ public class Maze {
         positionStart[3]= new Vector2(12,10);
         positionEnd = new Vector2(6,5);
         tab = new GameObject[width][height];
+        tab[0][0] = new Bomb(0,0,Textures.get("badlogic"));
+        tab[0][1] = new ActiveEnemy(1,0,Textures.get("badlogic"));
+        //tab[0][2] = new NumberBombBoost();
+        if(gson==null)createGson();
     }
 
-    public static Maze read(String filename) {
-        Maze m = null;
-        Gson g = new Gson();
+
+    public void draw(SpriteBatch batch){
+
+    }
+
+
+    public static Maze fromJsonFile(String filename) {
+        Maze m;
+        if(gson==null)createGson();
         try {
-            m = g.fromJson(new FileReader(new File(filename)), Maze.class);
-        } catch (FileNotFoundException e) { e.printStackTrace(); }
+            m = gson.fromJson(new FileReader(new File(filename)), Maze.class);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("ERROR : "+e.getMessage());
+        }
         return m;
     }
 
-    public String toJson(){
-        Gson g = new Gson();
-        return g.toJson(this);
+    public static Maze fromJson(String str) {
+        return gson.fromJson(str, Maze.class);
     }
 
-    public void draw(SpriteBatch batch){
+    public String toJson(){
+        return gson.toJson(this);
+    }
+
+    private static void createGson() {
+        gson = new GsonBuilder()
+                .registerTypeAdapter(GameObject.class, new PropertyBasedInterfaceMarshal())
+                .registerTypeAdapter(Bomb.class, new PropertyBasedInterfaceMarshal()).create();
+    }
+
+    public static class PropertyBasedInterfaceMarshal implements JsonSerializer<Object>, JsonDeserializer<Object> {
+        private static Gson gson = new Gson();
+        private static final String CLASS_META_KEY = "_class";
+
+        @Override
+        public Object deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
+            JsonObject jsonObj = (JsonObject) jsonElement;
+            String className = jsonObj.get(CLASS_META_KEY).getAsString();
+            try {
+                Class<?> clz = Class.forName(className);
+                return gson.fromJson(jsonElement, clz);
+            } catch (ClassNotFoundException e) {
+                throw new JsonParseException(e);
+            }
+        }
+
+
+        @Override
+        public JsonElement serialize(Object object, Type type, JsonSerializationContext jsonSerializationContext) {
+            System.out.println(object);
+            JsonElement jsonEle = gson.toJsonTree(object);
+            jsonEle.getAsJsonObject().addProperty(CLASS_META_KEY, object.getClass().getCanonicalName());
+            return jsonEle;
+        }
 
     }
 }
