28440473fa1cf27713a05918eb3f080df3fb0e1e
Rémy Cordeau-Mirani <rcordeau@enssat.fr>
2018-11-24 21:53:58 +0100

    ajout des classes Wall

---
+++ core/src/com/glhf/bomberball/IndestructibleWall.java
@@ -0,0 +1,11 @@
+package com.glhf.bomberball;
+
+import com.badlogic.gdx.graphics.Texture;
+
+public class IndestructibleWall extends  Wall {
+
+    // constructor
+    protected IndestructibleWall(int position_x, int position_y, Texture appearance) {
+        super(position_x, position_y, appearance);
+    }
+}
