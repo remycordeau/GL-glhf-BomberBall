c0eb8c164efa726436787a4609c4c1ccdd23f14e
julesl97 <jules.lagarde@gmail.com>
2018-11-25 14:01:16 +0100

    creation d'un fichier pour stocké toute les constantes du jeu les PATH pour image la largeur de l'écran etc

---
+++ core/src/com/glhf/bomberball/Constants.java
@@ -0,0 +1,14 @@
+package com.glhf.bomberball;
+
+public class Constants {
+    public static final int BOX_WIDTH = 16;
+    public static final int BOX_HEIGHT = 10;
+    public static final int APP_WIDTH = 4*200;
+    public static final int APP_HEIGHT = 3*200;
+
+
+    public static final String FLOOR_TEXTURE_NAME = "floor";
+
+
+    public static final String PATH_MAZE = "core/assets/maze/";
+}
