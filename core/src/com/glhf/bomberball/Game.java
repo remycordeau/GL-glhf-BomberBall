6e37f92a630bb9296c90c71c99c0a8a0ff740c69
thomas <tvoneuw@enssat.fr>
2018-11-25 09:59:12 +0100

    affichage image sans appui après merge

--- core/src/com/glhf/bomberball/Game.java
+++ core/src/com/glhf/bomberball/Game.java
@@ -4,32 +4,43 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.utils.Array;
+
+import java.io.File;
+import java.util.HashMap;
 
 public class Game extends ApplicationAdapter {
 	private SpriteBatch batch;
 	private BitmapFont font;
+	private Texture test;
+	private TextureRegion region;
+	private HashMap<String, Texture> textures;
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		font = new BitmapFont();
-		Textures.loadTextures();
+		test = new Texture(Gdx.files.internal("core/assets/img/badlogic.jpg"));
+		loadTextures();
+		region = new TextureRegion(test, 0, 0, 256, 256);
 		font.setColor(Color.RED);
-		//String t = new Maze().toJson();
-		//Maze tt = Maze.fromJson(t);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(0, 0, 0, 0);
+		Gdx.gl.glClearColor(255, 255, 255, 255);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		font.draw(batch, "Natan il est trop beau", 200, 200);
-        font.draw(batch, "Vincent trop stock", 500, 500);
-		font.draw(batch, "OBIIIIIIIIIIIIIIIIIIIIIIII", 300, 300);
-        batch.end();
+		batch.draw(region, 300, 200);
+		batch.end();
+
 	}
 	
 	@Override
@@ -38,4 +49,24 @@ public class Game extends ApplicationAdapter {
 		font.dispose();
 	}
 
+
+	private void loadTextures() {
+		textures = new HashMap<String, Texture>();
+		loadTextures("core/assets/");
+	}
+	private void loadTextures(String path) {
+		textures = new HashMap<String, Texture>();
+		File f = new File(path);
+		File[] files = f.listFiles();
+		assert files != null;// le fichier f doit être un dossier
+		for(File file : files) {
+			if (file.isDirectory()){
+				loadTextures(path + file.getName() + "/");
+				continue;
+			}
+			String stringId = file.getName().replace(".jpg", "");
+			System.out.println(stringId);
+			textures.put(stringId, new Texture(Gdx.files.internal(path + file.getName())));
+		}
+	}
 }
