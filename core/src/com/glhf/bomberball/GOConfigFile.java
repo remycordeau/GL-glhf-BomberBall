9abdfb6ba2a3e82ca608e5278032bedbaa462399
RomaneCastel <rcastel@enssat.fr>
2018-11-28 19:17:24 +0100

    creation .txt pour y stocker les variables modifiables par l'utilisateur

---
+++ core/src/com/glhf/bomberball/GOConfigFile.java
@@ -0,0 +1,15 @@
+package com.glhf.bomberball;
+import java.io.IOException;
+import java.io.*;
+
+public class GOConfigFile {
+    //attributes
+    private File file = new File("../../../../../assets/gameobject/config.txt");
+
+    public GOConfigFile(){
+
+    }
+    public String read(String variable){
+        return "bleuh";
+    }
+}
