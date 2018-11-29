2fc3c80603c008c96aeca0aa2bc9707558c7cae9
Cyril <cmoineau@enssat.fr>
2018-11-29 20:02:07 +0100

    Ajout de constante pour repérer les boutons. Overide de la methode touchDown ( NE FONCTIONNE PAS WORK IN PROGRESS !!!)

--- core/src/com/glhf/bomberball/menu/StateMainMenu.java
+++ core/src/com/glhf/bomberball/menu/StateMainMenu.java
@@ -1,21 +1,24 @@
 package com.glhf.bomberball.menu;
 
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
 import com.glhf.bomberball.Graphics;
 
-import static com.glhf.bomberball.Graphics.GUI.get;
 
 public class StateMainMenu extends State {
 
     //Attributes
     private TextureAtlas.AtlasRegion solo;
-    private TextureRegion region1;
     private TextureAtlas.AtlasRegion multi;
-    private TextureRegion region2;
     private TextureAtlas.AtlasRegion parametre;
-    private TextureRegion region3;
+    private TextureAtlas.AtlasRegion erreur;
+    private final int X_BUTTON =200;
+    private final int Y_SOLO=300;
+    private final int Y_MULTI=200;
+    private final int Y_PARAM=100;
+    private final int X_SIZE=376;  //Note : les boutons à la rache font 376x183
+    private final int Y_SIZE=183;
+
 
     //Constructor
     public StateMainMenu(){
@@ -24,22 +27,38 @@ public class StateMainMenu extends State {
     }
     /*Loading textures*/
     public void settings(){
-        //BoutonSolo
-        //get("BoutonSolo");
-        solo = Graphics.GUI.get("boutonSolo");
 
+        //Message d'erreur
+        erreur = Graphics.GUI.get("erreur");
+        //BoutonSolo
+        solo = Graphics.GUI.get("boutonSolo");
         //BoutonMulti
         multi = Graphics.GUI.get("boutonMulti");
-
         //Bouton Paramètres
         parametre = Graphics.GUI.get("boutonParametre");
     }
 
     public void draw(){
         batch.begin();
-        batch.draw(solo, 200, 300);
-        batch.draw(multi, 200, 200);
-        batch.draw(parametre, 200, 100);
+        batch.draw(solo, X_BUTTON, Y_SOLO);
+        batch.draw(multi, X_BUTTON, Y_MULTI);
+        batch.draw(parametre, X_BUTTON, Y_PARAM);
         batch.end();
+    }
+    @Override
+    public boolean touchDown(int x, int y, int pointer, int bouton) {
+        if(x> X_BUTTON && x < X_BUTTON+X_SIZE && y> Y_SOLO && y<Y_SOLO+Y_SIZE)
+        {
+            //TODO: On lance le jeu solo
+        }
+        if(x> X_BUTTON && x < X_BUTTON+X_SIZE && y> Y_MULTI && y<Y_MULTI+Y_SIZE)
+        {
+            //TODO: On lance le jeu multi
+        }
+        if(x> X_BUTTON && x < X_BUTTON+X_SIZE && y> Y_PARAM && y<Y_PARAM+Y_SIZE)
+        {
+            //TODO: On lance les parametres
+        }
+        return false;
     }
 }
