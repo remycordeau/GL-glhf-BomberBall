ed0648d6a14ad2061b6186c1a3259d5ea8f90036
thomas <tvoneuw@enssat.fr>
2018-11-29 17:32:48 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	core/assets/graphics/packs/pack_animations.png

--- core/src/com/glhf/bomberball/menu/StateMainMenu.java
+++ core/src/com/glhf/bomberball/menu/StateMainMenu.java
@@ -1,16 +1,18 @@
 package com.glhf.bomberball.menu;
 
+import com.badlogic.gdx.Graphics;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class StateMainMenu extends State {
 
     //Attributes
-    private Texture test1;
+    private TextureAtlas.AtlasRegion test1;
     private TextureRegion region1;
-    private Texture test2;
+    private TextureAtlas.AtlasRegion test2;
     private TextureRegion region2;
-    private Texture test3;
+    private TextureAtlas.AtlasRegion test3;
     private TextureRegion region3;
 
     //Constructor
@@ -22,15 +24,15 @@ public class StateMainMenu extends State {
     /*Loading textures*/
     public void settings(){
         //BoutonSolo
-        test1 = Texture.get("BoutonSolo");//TODO La texture boutonsolo n'a pas été push
+        test1 = Graphics.GUI.get("BoutonSolo");
         region1 = new TextureRegion(test1, 0, 0, 400, 100);
 
         //BoutonMulti
-        test2 = Textures.get("BoutonMulti");
+        test2 = Graphics.GUI.get("BoutonMulti");
         region2 = new TextureRegion(test2, 0, 0, 400, 100);
 
         //Bouton Paramètres
-        test3 = Textures.get("BoutonParametres");
+        test3 = Graphics.GUI.get("BoutonParametres");
         region3 = new TextureRegion(test3, 0, 0, 400, 100);
     }
 
