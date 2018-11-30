8abac8c4f956aebbdd4db5130525584273f06266
thomas <tvoneuw@enssat.fr>
2018-11-30 13:52:31 +0100

    Factorisation de code pour la classe bouton

--- core/src/com/glhf/bomberball/menu/StateMainMenu.java
+++ core/src/com/glhf/bomberball/menu/StateMainMenu.java
@@ -1,26 +1,16 @@
 package com.glhf.bomberball.menu;
 
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-
 import com.glhf.bomberball.Constants;
+import com.glhf.bomberball.GUI.Button;
 import com.glhf.bomberball.Game;
 import com.glhf.bomberball.Graphics;
 
 
 public class StateMainMenu extends State {
-
     //Attributes
-    private TextureAtlas.AtlasRegion solo;
-    private TextureAtlas.AtlasRegion multi;
-    private TextureAtlas.AtlasRegion parametre;
-    private TextureAtlas.AtlasRegion erreur;
-    private final int X_BUTTON =160;
-    private final int Y_SOLO=300;
-    private final int Y_MULTI=200;
-    private final int Y_PARAM=100;
-    private final int X_SIZE=400;  //Note : les boutons à la rache font 376x183
-    private final int Y_SIZE=100;
-
+    private Button solo;
+    private Button multi;
+    private Button parametres;
 
     //Constructor
     public StateMainMenu(String name){
@@ -30,43 +20,34 @@ public class StateMainMenu extends State {
 
     /*Loading textures*/
     public void settings(){
-
-        //Message d'erreur
-        erreur = Graphics.GUI.get("erreur");
         //BoutonSolo
-        solo = Graphics.GUI.get("BoutonSolo");
+        solo = new Button(160, 300, 400, 100, "BoutonSolo");
         //BoutonMulti
-        multi = Graphics.GUI.get("BoutonMulti");
-        //Bouton Paramètres
-        parametre = Graphics.GUI.get("BoutonParametres");
+        multi = new Button(160, 200, 400, 100, "BoutonMulti");
+        //BoutonParametres
+        parametres = new Button(160, 100, 400, 100, "BoutonParametres");
     }
 
     public void draw(){
-        batch.begin();
-        batch.draw(solo, X_BUTTON, Y_SOLO);
-        batch.draw(multi, X_BUTTON, Y_MULTI);
-        batch.draw(parametre, X_BUTTON, Y_PARAM);
-        batch.end();
+        solo.draw(batch);
+        multi.draw(batch);
+        parametres.draw(batch);
     }
 
     @Override
     public boolean touchDown(int x, int y, int pointer, int button) {
         int y_screen = Constants.APP_HEIGHT - y;
-        if(x> X_BUTTON && x < X_BUTTON+X_SIZE && y_screen > Y_SOLO && y_screen < Y_SOLO+Y_SIZE)
-
-        {
+        if(solo.contains(x, y)) {
             //TODO: On lance le jeu solo
         }
-        if(x> X_BUTTON && x < X_BUTTON+X_SIZE && y_screen > Y_MULTI && y_screen <Y_MULTI+Y_SIZE)
+        if(multi.contains(x, y))
         {
             //TODO: On lance le jeu multi
             State state = new StateGameMulti("classic_maze_1.json");
             Game.setState(state);
         }
-        if(x> X_BUTTON && x < X_BUTTON+X_SIZE && y_screen > Y_PARAM && y_screen <Y_PARAM+Y_SIZE)
-        {
+        if(solo.contains(x, y)) {
             //TODO: On lance les parametres
-
         }
         return false;
     }
