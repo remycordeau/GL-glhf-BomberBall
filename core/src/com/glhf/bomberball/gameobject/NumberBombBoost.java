8ab07d6d8c30a4d1a4b6a7cfe4c13c8225d8ea3b
Natan <ntalon@enssat.fr>
2018-11-30 13:51:50 +0100

    ajout de java doc remonté de l'animation dans character

--- core/src/com/glhf/bomberball/gameobject/NumberBombBoost.java
+++ core/src/com/glhf/bomberball/gameobject/NumberBombBoost.java
@@ -1,10 +1,15 @@
 package com.glhf.bomberball.gameobject;
 
 public class NumberBombBoost extends Bonus {
-    //constructor
+
+    /**
+     * constructor
+     * @param position_x x axis initial position
+     * @param position_y y axis initial position
+     */
     public NumberBombBoost(int position_x, int position_y){
         super(position_x, position_y);
         name="NumberBombBoost";
-        //appearance
+        //TODO set the sprite of the NumberBombBoost
     }
 }
