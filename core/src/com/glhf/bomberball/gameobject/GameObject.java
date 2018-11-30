94b436a860799c0a24f6633faadf125896fcb801
RomaneCastel <rcastel@enssat.fr>
2018-11-30 13:49:21 +0100

    javadoc again

--- core/src/com/glhf/bomberball/gameobject/GameObject.java
+++ core/src/com/glhf/bomberball/gameobject/GameObject.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas.AtlasRegion;
 import com.glhf.bomberball.Constants;
+import com.sun.xml.internal.bind.v2.TODO;
 
 import java.io.Serializable;
 
@@ -58,18 +59,28 @@ public abstract class GameObject {
         this.position_x = position_x;
     }
 
-
+    /**
+     * set a value for the y axis position
+     * @param position_y
+     */
     public void setPositionY(int position_y) {
         this.position_y = position_y;
     }
 
+    /**
+     * modification of the life of the gameObject
+     * @param damage
+     */
     public void getDamage(int damage){
         life-=damage;
     }
 
     public AtlasRegion getSprite() { return this.sprite; }
 
-    // is the Object alive ?
+    /**
+     * to know if an object has been destroyed or not
+     * @return boolean if the gameObject is Alive
+     */
     public boolean isAlive() {
         return life > 0;
     }
