6967d51ba152f00458d7cb0835bd214d80d554d9
thomas <tvoneuw@enssat.fr>
2018-12-01 16:20:14 +0100

    Ajout setStringAttribute (casi idem setIntAttribute)

--- core/src/com/glhf/bomberball/ConfigFile.java
+++ core/src/com/glhf/bomberball/ConfigFile.java
@@ -137,6 +137,89 @@ public class ConfigFile {
         }
     }
 
+    public void setStringAttribute(String attributeName, String newString){
+        //Writing from config.txt to configtmp.txt with new settings
+        try{
+            this.scanner = new Scanner(this.file);
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
+
+        FileWriter fw = null;
+
+        String s = "";
+        String stmp = "";
+        while(scanner.hasNextLine()){
+            try{
+                fw = new FileWriter(Constants.PATH_CONFIG_TMP, true);
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+            stmp = scanner.findInLine(attributeName);
+            if(stmp != null){
+                stmp += " " + newString;
+                scanner.next();
+            }
+            s = scanner.nextLine();
+            try {
+                if(stmp != null) {
+                    fw.write(stmp + s + "\n");
+                    fw.close();
+                }else {
+                    fw.write(s + "\n");
+                    fw.close();
+                }
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+        scanner.close();
+
+        //Writing from configtmp.txt to config.txt (copy)
+        Scanner scannercopy = null;
+        try {
+            scannercopy = new Scanner(new File(Constants.PATH_CONFIG_TMP));
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
+
+        //Borrow old file config.txt
+        try{
+            fw = new FileWriter(this.file, false);
+            fw.write("");
+            fw.close();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+        //Copying
+        s = "";
+        while(scannercopy.hasNextLine()){
+            try{
+                fw = new FileWriter(this.file, true);
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+            s = scannercopy.nextLine();
+            try {
+                fw.write(s + "\n");
+                fw.close();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+        scannercopy.close();
+
+        //Reset configtmp.txt
+        try{
+            fw = new FileWriter(Constants.PATH_CONFIG_TMP, false);
+            fw.write("");
+            fw.close();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
     @Override
     public void finalize() {
         this.scanner.close();
