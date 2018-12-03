bc48a3f995a7985d123e31daae660dacb82ad9c4
RomaneCastel <rcastel@enssat.fr>
2018-12-03 19:23:51 +0100

    creation classe PlayerInfo pour l'interface multi

---
+++ core/src/com/glhf/bomberball/interfaceMulti/PlayerInfo.java
@@ -0,0 +1,21 @@
+package com.glhf.bomberball.interfaceMulti;
+
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.glhf.bomberball.gameobject.Player;
+
+public class PlayerInfo extends Actor {
+    //attributes
+    private Player player;
+
+    //constructor
+    public PlayerInfo(Player player) {
+        this.player = player;
+    }
+
+    @Override
+    public void draw(Batch batch, float parentAlpha) {
+        super.draw(batch, parentAlpha);
+
+    }
+}
