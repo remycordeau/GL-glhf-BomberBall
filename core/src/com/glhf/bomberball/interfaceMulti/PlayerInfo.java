ba80fbaadfc9b5d2a15c6e9a7cff22311e8d0001
RomaneCastel <rcastel@enssat.fr>
2018-12-06 15:38:26 +0100

    sideBar presque finie manque juste skin pour label et scaling

--- core/src/com/glhf/bomberball/interfaceMulti/PlayerInfo.java
+++ core/src/com/glhf/bomberball/interfaceMulti/PlayerInfo.java
@@ -7,26 +7,6 @@ import com.glhf.bomberball.Graphics;
 import com.glhf.bomberball.gameobject.Player;
 
 public class PlayerInfo extends HorizontalGroup {
-    /*
-    //attributes
-    private Player player;
-
-    //constructor
-    public PlayerInfo(Player player) {
-        this.player = player;
-        TextureAtlas.AtlasRegion atlas_region = player.getSprite();
-        this.setBounds(atlas_region.getRegionX(), atlas_region.getRegionY(), atlas_region.getRegionWidth(), atlas_region.getRotatedPackedHeight());
-
-    }
-
-    @Override
-    public void draw(Batch batch, float parentAlpha) {
-        batch.draw(player.getSprite(), this.getX(), this.getY());
-        batch.draw(Graphics.Sprites.get("bomb"),this.getX()+20f, this.getY());
-        for(int i=2; i<=player.getLife()+1; i++) {
-            batch.draw(Graphics.Sprites.get("ui_heart_full"), this.getX() + i*20f, this.getY());
-        }
-    }*/
 
     //attributes
     private Player player;
@@ -43,14 +23,17 @@ public class PlayerInfo extends HorizontalGroup {
     public PlayerInfo(Player player){
         this.player = player;
         info_player = new VerticalGroup();
-        heart_group = new HorizontalGroup();
-        bonus_group = new HorizontalGroup();
+        info_player.setDebug(true);
+        heart_group = new HorizontalGroup().space(10f);
+        bonus_group = new HorizontalGroup().space(10f);
         player_profil = new Image(player.getSprite());
         this.addActor(player_profil);
         this.addActor(info_player);
         info_player.addActor(heart_group);
+        heart_group.setDebug(true);
         this.addHeart();
         info_player.addActor(bonus_group);
+        bonus_group.setDebug(true);
         this.addBonus();
     }
 
