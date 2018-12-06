b7908c7280c745654747a8791c70c63f19b04765
Cyril <cmoineau@enssat.fr>
2018-12-06 18:46:06 +0100

    Passage des boutons en attributs pour plus de propreté !

--- core/src/com/glhf/bomberball/menu/StateMultiMenu.java
+++ core/src/com/glhf/bomberball/menu/StateMultiMenu.java
@@ -3,30 +3,38 @@ package com.glhf.bomberball.menu;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.VerticalGroup;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.glhf.bomberball.Constants;
 import com.glhf.bomberball.Game;
 import com.glhf.bomberball.Graphics;
+import com.glhf.bomberball.maze.Maze;
+import com.glhf.bomberball.maze.MazeDrawer;
+
+import javax.xml.soap.Text;
 
 public class StateMultiMenu extends StateMenu {
     //Attributes
-    StateMainMenu mainMenu;
+    private StateMainMenu mainMenu;
+    private String previewFile="maze_0.json";
+    protected VerticalGroup previewButtons;
+    protected TextButton nextMapButton;
+    protected TextButton previousMapButton;
+    protected TextButton playButton;
+    protected TextButton cancelButton;
 
-    //Constructor
-    public StateMultiMenu(StateMainMenu mainMenu)
-    {
+    public StateMultiMenu(StateMainMenu mainMenu) {
         super();
         this.mainMenu = mainMenu;
         initializeButtons();
     }
 
     public void initializeButtons(){
-        TextButton textButton = new TextButton("Jouer", Graphics.GUI.getSkin());
-        textButton.addListener(new SetStateListener(new StateGameMulti("maze_0.json")));
-        centerButtons.addActor(textButton);
-
-        textButton = new TextButton("Retour", Graphics.GUI.getSkin());
-        textButton.addListener(new SetStateListener(mainMenu));
-        centerButtons.addActor(textButton);
+        playButton = new TextButton("Jouer", Graphics.GUI.getSkin());
+        playButton.addListener(new SetStateListener(new StateGameMulti(previewFile)));
+        centerButtons.addActor(playButton);
+        cancelButton = new TextButton("Retour", Graphics.GUI.getSkin());
+        cancelButton.addListener(new SetStateListener(mainMenu));
+        centerButtons.addActor(cancelButton);
     }
 }
