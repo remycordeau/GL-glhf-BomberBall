8ddc2457642409ff96017c2fd4033a4de8d848ea
Cyril <cmoineau@enssat.fr>
2018-12-06 19:10:56 +0100

    Ajout de boutons pour choisir le labyrinthe sur lequel jouer.

--- core/src/com/glhf/bomberball/menu/StateMultiMenu.java
+++ core/src/com/glhf/bomberball/menu/StateMultiMenu.java
@@ -2,6 +2,7 @@ package com.glhf.bomberball.menu;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.ui.HorizontalGroup;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.VerticalGroup;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
@@ -17,7 +18,7 @@ public class StateMultiMenu extends StateMenu {
     //Attributes
     private StateMainMenu mainMenu;
     private String previewFile="maze_0.json";
-    protected VerticalGroup previewButtons;
+    protected HorizontalGroup previewButtons;
     protected TextButton nextMapButton;
     protected TextButton previousMapButton;
     protected TextButton playButton;
@@ -36,5 +37,21 @@ public class StateMultiMenu extends StateMenu {
         cancelButton = new TextButton("Retour", Graphics.GUI.getSkin());
         cancelButton.addListener(new SetStateListener(mainMenu));
         centerButtons.addActor(cancelButton);
+
+        // Display of the preview
+            //TODO: arriver à afficher le labyrinthe ...
+        Maze maze = Maze.fromJsonFile(previewFile);
+        MazeDrawer maze_drawer = new MazeDrawer(maze, 0f,1f,0f,1f, MazeDrawer.Fit.BEST);
+        maze_drawer.drawMaze();
+
+        //Buttons to chose the maze you want to play in
+            //TODO: Placer les boutons à droite et à gauche de l'écran (utiliser deux horizontal group différents ?)
+        previewButtons=new HorizontalGroup();
+        previewButtons.setFillParent(true);
+        nextMapButton = new TextButton(">", Graphics.GUI.getSkin());
+        previousMapButton = new TextButton("<", Graphics.GUI.getSkin());
+        previewButtons.addActor(previousMapButton);
+        previewButtons.addActor(nextMapButton);
+        stage.addActor(previewButtons);
     }
 }
