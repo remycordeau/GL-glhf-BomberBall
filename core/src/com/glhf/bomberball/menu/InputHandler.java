38a74b4e3144291077a946c74bc97af544071a5c
nayala <nayala@enssat.fr>
2018-12-07 16:46:57 +0100

    class InputHandler + javadoc

--- core/src/com/glhf/bomberball/menu/InputHandler.java
+++ core/src/com/glhf/bomberball/menu/InputHandler.java
@@ -7,22 +7,29 @@ import com.glhf.bomberball.config.InputsConfig;
 /**
  * class InputHandler
  *
- * Class to handle inputs, use it to register input action handlers.
+ * Class to handle inputs, use it to register input handlers on actions.
  *
  * Example :
  *
  * inputHandler.registerKeyAction(KeyAction.KEY_UP, () -> { keyUpHandler() })
- * to register an action on KEY_UP
+ * to register keyUpHandler as an handler on KEY_UP action
  *
  * inputHandler.registerButtonAction(ButtonAction.BUTTON_LEFT, (x, y) -> { buttonLeftHandler(x, y) })
- * to register an action on BUTTON_LEFT
+ * to register buttonLeftHandler as an handler on BUTTON_LEFT action
  *
  * @author nayala
  */
 public class InputHandler extends InputListener {
 
+    /**
+     * InputsConfig class to link actions to codes
+     */
     private InputsConfig inputs_config;
 
+    /**
+     * KeyActions
+     * All registrable key actions
+     */
     public enum KeyAction {
         KEY_UP,
         KEY_DOWN,
@@ -31,6 +38,10 @@ public class InputHandler extends InputListener {
         KEY_SPACE
     }
 
+    /**
+     * Buttons
+     * All registrable button actions
+     */
     public enum ButtonAction {
         BUTTON_LEFT,
         BUTTON_RIGHT
@@ -39,11 +50,18 @@ public class InputHandler extends InputListener {
     private KeyActionHandler[] key_handlers = new KeyActionHandler[KeyAction.values().length];
     private ButtonActionHandler[] button_handlers = new ButtonActionHandler[KeyAction.values().length];
 
+    /**
+     * Default constructor
+     * Creates an InputHandler with default input codes configurations
+     */
     public InputHandler() {
         // TODO : importer la bonne config d'inputs
         inputs_config = InputsConfig.defaultConfig();
     }
 
+    /**
+     * You shouln't use this method
+     */
     @Override
     public boolean keyDown(InputEvent event, int keycode) {
         if (inputs_config.isKeyCodeAssigned(keycode)) {
@@ -55,6 +73,9 @@ public class InputHandler extends InputListener {
         return false;
     }
 
+    /**
+     * You shouln't use this method
+     */
     @Override
     public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
         if (inputs_config.isButtonCodeAssigned(button)) {
@@ -66,17 +87,38 @@ public class InputHandler extends InputListener {
         return false;
     }
 
+    /**
+     * Registers a key action.
+     * @param key_action action to register
+     * @param key_handler handler to call when key_action is triggered
+     */
     public void registerKeyAction(KeyAction key_action, KeyActionHandler key_handler) {
         key_handlers[key_action.ordinal()] = key_handler;
     }
+
+    /**
+     * Registers a button action.
+     * @param button_action action to register
+     * @param button_handler handler to call when button_action is triggered
+     */
     public void registerButtonAction(ButtonAction button_action, ButtonActionHandler button_handler) {
         button_handlers[button_action.ordinal()] = button_handler;
     }
 
+    /**
+     * Interface to handle key actions.
+     * Use lambda functions :
+     * KeyActionHandler key_handler = () -> { handle_key_action(); };
+     */
     public interface KeyActionHandler {
         void handle();
     }
 
+    /**
+     * Interface to handle button actions.
+     * Use lambda functions :
+     * KeyActionHandler button_handler = (x, y) -> { handle_button_action(x, y); };
+     */
     public interface ButtonActionHandler {
         void handle(float x, float y);
     }
