f0016b2acc43f85726fd2389b8292e38d0e11611
lagarde <jlagarde@enssat.fr>
2018-12-11 23:05:22 +0100

    modification fichier de config pour correspondre au diagramme modification du menu de paramètres

--- core/src/com/glhf/bomberball/config/GameSoloConfig.java
+++ core/src/com/glhf/bomberball/config/GameSoloConfig.java
@@ -4,12 +4,16 @@ public class GameSoloConfig extends GameConfig {
 
     public String player_skin = "knight_m";
 
+    // Enemies
     public int activeEnemy_life = 1;
     public int activeEnemy_strength = 1;
+    public int activeEnemy_moves = 5;
 
     public int passiveEnemy_life = 1;
     public int passiveEnemy_strength = 1;
+    public int passiveEnemy_moves = 5;
 
     public int aggressiveEnemy_life = 1;
     public int aggressiveEnemy_strength = 1;
+    public int aggressiveEnemy_moves = 5;
 }
--- core/src/com/glhf/bomberball/screens/SettingsMenuScreen.java
+++ core/src/com/glhf/bomberball/screens/SettingsMenuScreen.java
@@ -1,15 +1,35 @@
 package com.glhf.bomberball.screens;
 
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Event;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.glhf.bomberball.Graphics;
+import com.glhf.bomberball.Graphics.GUI;
+import com.glhf.bomberball.config.GameConfig;
+
+import java.lang.reflect.Field;
 
 public class SettingsMenuScreen extends MenuScreen {
 
     //Constructor
     public SettingsMenuScreen() {
         super();
-        centerButtons.addActor(new ParameterString("skin of player1"));
         centerButtons.addActor(new ParameterInt("number of players", 1, 4, 1));
+        for (Field field : GameConfig.class.getDeclaredFields()) {
+            if(field.getType().equals(String.class)){
+                centerButtons.addActor(new ParameterString(field.getName()));
+            }else if(field.getType().equals(int.class)){
+                centerButtons.addActor(new ParameterInt(field.getName(),1,10,1));
+            }else {
+
+            }
+        }
+        TextButton cancelButton = new TextButton("Retour", Graphics.GUI.getSkin());
+        cancelButton.addListener(new ScreenChangeListener(MainMenuScreen.class));
+        centerButtons.addActor(cancelButton);
+
     }
 
     public abstract class Parameter extends HorizontalGroup {
@@ -32,10 +52,20 @@ public class SettingsMenuScreen extends MenuScreen {
         }
     }
     public class ParameterInt extends Parameter {
-        private Slider value;
+        private HorizontalGroup value;
         public ParameterInt(String name, float min, float max, float step) {
             super(name);
-            value = new Slider(min, max, step, false, Graphics.GUI.getSkin());
+            value = new HorizontalGroup();
+            Slider slider = new Slider(min, max, step, false, GUI.getSkin());
+            Label label = new Label("0", GUI.getSkin());
+            slider.addListener(new ChangeListener() {
+                @Override
+                public void changed(ChangeEvent event, Actor actor) {
+                    label.setText(""+(int)((Slider)actor).getValue());
+                }
+            });
+            value.addActor(label);
+            value.addActor(slider);
             addActor(value);
         }
     }
