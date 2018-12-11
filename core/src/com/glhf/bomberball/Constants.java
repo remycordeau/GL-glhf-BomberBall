16194fe51d8fc9c99ac318cd596cdb497157d0cb
nayala <nayala@enssat.fr>
2018-12-11 20:25:30 +0100

    nettoyage class Constants

--- core/src/com/glhf/bomberball/Constants.java
+++ core/src/com/glhf/bomberball/Constants.java
@@ -10,20 +10,10 @@ public class Constants {
     // constans for paths
     public static final String PATH_ASSET         = "core/assets/";
     public static final String PATH_MAZE          = PATH_ASSET+"maze/";
-    public static final String PATH_CONFIG_FILE   = PATH_ASSET+"config.json";
-    public static final String PATH_GAMEOBJECT    = PATH_ASSET+"Config/config.txt";
-    public static final String PATH_CONFIG_TMP    = PATH_ASSET+"Config/configtmp.txt";
     public static final String PATH_GRAPHICS      = PATH_ASSET+"graphics/";
     public static final String PATH_FONTS         = PATH_GRAPHICS+"fonts/";
     public static final String PATH_PACKS         = PATH_GRAPHICS+"packs/";
     public static final String PATH_ATLAS_SPRITES = PATH_PACKS+"pack_sprites.atlas";
     public static final String PATH_ATLAS_ANIMS   = PATH_PACKS+"pack_animations.atlas";
     public static final String PATH_ATLAS_GUI     = PATH_PACKS+"pack_gui.atlas";
-
-    // contants for assets
-    public static final int NB_FLOOR_VARIATION = 8;
-
-    // constants for enemies
-    public enum moves {UP, DOWN, RIGHT, LEFT}
-    public enum enemyState {ACTIVE, HUNTER}
 }
