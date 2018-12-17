8742ebb93385f59aa5c158b9088d1c83da7824e2
Cyril <cmoineau@enssat.fr>
2018-12-17 13:57:44 +0100

    Remplissage des fonctions nextplayer.

--- core/src/com/glhf/bomberball/screens/MultiMenuScreen.java
+++ core/src/com/glhf/bomberball/screens/MultiMenuScreen.java
@@ -13,16 +13,22 @@ public class MultiMenuScreen extends AbstractScreen {
     public Maze maze;
     public AnimationActor[] selectPlayer=
             {
-            new AnimationActor(new Animation<TextureAtlas.AtlasRegion>(0.15f, Graphics.Anims.get("knight_m/idle"), Animation.PlayMode.LOOP)),
-            new AnimationActor(new Animation<TextureAtlas.AtlasRegion>(0.15f, Graphics.Anims.get("knight_f/idle"), Animation.PlayMode.LOOP)),
-            new AnimationActor(new Animation<TextureAtlas.AtlasRegion>(0.15f, Graphics.Anims.get("elf_f/idle"), Animation.PlayMode.LOOP)),
-            new AnimationActor(new Animation<TextureAtlas.AtlasRegion>(0.15f, Graphics.Anims.get("elf_m/idle"), Animation.PlayMode.LOOP)),
+            new AnimationActor(new Animation<TextureAtlas.AtlasRegion>(0.15f, Graphics.Anims.get("knight_m/idle"), Animation.PlayMode.LOOP)),   //0
+            new AnimationActor(new Animation<TextureAtlas.AtlasRegion>(0.15f, Graphics.Anims.get("knight_f/idle"), Animation.PlayMode.LOOP)),   //1
+            new AnimationActor(new Animation<TextureAtlas.AtlasRegion>(0.15f, Graphics.Anims.get("elf_f/idle"), Animation.PlayMode.LOOP)),  //2
+            new AnimationActor(new Animation<TextureAtlas.AtlasRegion>(0.15f, Graphics.Anims.get("elf_m/idle"), Animation.PlayMode.LOOP)),  //3
             new AnimationActor(new Animation<TextureAtlas.AtlasRegion>(0.15f, Graphics.Anims.get("wizzard_m/idle"), Animation.PlayMode.LOOP)),
-            new AnimationActor(new Animation<TextureAtlas.AtlasRegion>(0.15f, Graphics.Anims.get("wizzard_f/idle"), Animation.PlayMode.LOOP))
+            new AnimationActor(new Animation<TextureAtlas.AtlasRegion>(0.15f, Graphics.Anims.get("wizzard_f/idle"), Animation.PlayMode.LOOP)),
+            new AnimationActor(new Animation<TextureAtlas.AtlasRegion>(0.15f, Graphics.Anims.get("skelet/idle"), Animation.PlayMode.LOOP)) // Le personnage ayant cette animation ne sera pas inclus dans
     };
 
     private int maze_id = 0;
     private final int maze_count = 7;
+
+    public int p1_id=0;
+    public int p2_id=2;
+    public int p3_id=4;
+    public int p4_id=6;
 
     public MultiMenuScreen() {
         maze = Maze.importMaze("maze_" + maze_id);
@@ -39,18 +45,37 @@ public class MultiMenuScreen extends AbstractScreen {
         maze = Maze.importMaze("maze_" + maze_id);
     }
     public void nextP1(){
-
+        p1_id++;
+        while (p1_id==p2_id || p1_id==p3_id || p1_id==p4_id)
+        {
+            p1_id++;
+            if (p1_id==6) { p1_id=0;}
+        }
     }
     public void nextP2(){
-
+        p2_id++;
+        while (p2_id==p1_id || p2_id==p3_id || p2_id==p4_id)
+        {
+            p2_id++;
+            if (p2_id==6) { p2_id=0;}
+        }
     }
     public void nextP3(){
-
+        p3_id++;
+        while (p3_id==p2_id || p1_id==p3_id || p3_id==p4_id)
+        {
+            p3_id++;
+            if (p3_id==7) { p3_id=0;}
+        }
     }
     public void nextP4(){
-
+        p4_id++;
+        while (p4_id==p2_id || p4_id==p3_id || p3_id==p4_id)
+        {
+            p4_id++;
+            if (p4_id==6) { p4_id=0;}
+        }
     }
-
 
     public int getMazeId() {
         return maze_id;
