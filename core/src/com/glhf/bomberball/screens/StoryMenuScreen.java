694961278e6158d45ad26215a80273d834ecc3e0
Rémy Cordeau-Mirani <rcordeau@enssat.fr>
2018-12-24 13:21:11 +0100

    Prévisualisation des niveaux Solo (pour le moment avec les maps multi)

--- core/src/com/glhf/bomberball/screens/StoryMenuScreen.java
+++ core/src/com/glhf/bomberball/screens/StoryMenuScreen.java
@@ -1,11 +1,41 @@
 package com.glhf.bomberball.screens;
 
+import com.glhf.bomberball.maze.Maze;
 import com.glhf.bomberball.ui.StoryMenuUI;
+
+import static java.lang.Math.abs;
 
 public class StoryMenuScreen extends MenuScreen {
 
+    public Maze maze;
+    private int maze_id = 0;
+    private final int maze_count = 7;
+
     public StoryMenuScreen(){
         super();
-        addUI(new StoryMenuUI());
+        maze = Maze.importMaze("maze_" + maze_id);
+        addUI(new StoryMenuUI(this));
     }
+
+    /**
+     * Change the maze to the next one in the maze list
+     */
+    public void nextMaze(){
+        maze_id = (maze_id + 1) % maze_count;
+        maze = Maze.importMaze("maze_" + maze_id);
+    }
+
+    /**
+     * Change the maze to the previous one in the maze list
+     */
+    public void previousMaze(){
+        maze_id = (maze_count + maze_id - 1) % maze_count;
+        maze = Maze.importMaze("maze_" + maze_id);
+    }
+
+    public int getMazeId(){
+        return maze_id;
+    }
+
+
 }
--- core/src/com/glhf/bomberball/ui/StoryMenuUI.java
+++ core/src/com/glhf/bomberball/ui/StoryMenuUI.java
@@ -1,59 +1,85 @@
 package com.glhf.bomberball.ui;
 
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.scenes.scene2d.ui.Value;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.utils.Align;
+import com.glhf.bomberball.Bomberball;
 import com.glhf.bomberball.Graphics;
-import com.glhf.bomberball.screens.MainMenuScreen;
-import com.glhf.bomberball.screens.ScreenChangeListener;
+import com.glhf.bomberball.maze.MazeDrawer;
+import com.glhf.bomberball.screens.*;
 
 public class StoryMenuUI extends Table {
 
-    private TextButton back_button, next_level_button, previous_level_button;
+    private TextButton back_button, next_level_button, previous_level_button,play_button;
     private Label label;
     private Table level_selection;
+    private MazeDrawer level_preview;
+    private StoryMenuScreen screen;
+    private Table buttons;
 
 
-    public StoryMenuUI(){
+    public StoryMenuUI(StoryMenuScreen screen){
 
         super();
-
+        this.screen = screen;
         this.setFillParent(true);
-        //this.padLeft(Value.percentWidth(0.25f));
-        //this.padRight(Value.percentWidth(0.25f));
-//        this.padTop(Value.percentHeight(0.1f));
-        this.padBottom(Value.percentHeight(0.5f));
         level_selection = new Table();
-
-
         addButtons();
+        level_preview = new MazeDrawer(screen.maze, 0.25f, 0.75f,  0.5f, 0.75f, MazeDrawer.Fit.BEST);
+        this.add(level_preview);
     }
+
+    /**
+     * initializes and adds all the buttons to the table. Also adds listeners to buttons if necessary
+     */
 
     private void addButtons() {
 
         label = new Label("Level Selection",Graphics.GUI.getSkin(),"Title");
         label.setAlignment(Align.center);
-        this.add(label).growX().spaceBottom(Value.percentHeight(1)).row();
+        label.setFontScale(1.7f,1.7f);
+        this.add(label).row();
 
 
         previous_level_button = new TextButton("<",Graphics.GUI.getSkin());
+        previous_level_button.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                screen.previousMaze();
+                level_preview.setMaze(screen.maze);
+            }
+        });
         level_selection.add(previous_level_button);
 
         next_level_button = new TextButton(">",Graphics.GUI.getSkin());
+        next_level_button.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                screen.nextMaze();
+                level_preview.setMaze(screen.maze);
+            }
+        });
         level_selection.add(next_level_button).spaceLeft(Value.percentHeight(8f));
 
-        this.add(level_selection).row();
+        this.add(level_selection).align(Align.center).grow().row();
+
+        buttons = new Table();
+        play_button = new TextButton("Play Selected Level",Graphics.GUI.getSkin());
+        play_button.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                Bomberball.changeScreen(new GameMultiScreen(screen.maze,screen.getMazeId()));
+            }
+        });
+        buttons.add(play_button).row();
 
         back_button = new TextButton("Back to main menu",Graphics.GUI.getSkin());
-        back_button.getLabel().setFontScale(0.8f,0.8f);
         back_button.addListener(new ScreenChangeListener(MainMenuScreen.class));
-        this.add(back_button).bottom();
-
-
-
-        }
+        back_button.getLabel().setFontScale(0.8f,0.8f);
+        buttons.add(back_button).spaceTop(Value.percentHeight(1f));
+        this.add(buttons);
+    }
 }
 
 
