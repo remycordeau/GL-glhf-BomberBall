fa7d98f3674fbcb70a12456dcdec64acda0e38dd
RomaneCastel <rcastel@enssat.fr>
2019-01-07 21:54:20 +0100

    petite correction sur les Constantes

--- core/src/com/glhf/bomberball/gameobject/PassiveEnemy.java
+++ core/src/com/glhf/bomberball/gameobject/PassiveEnemy.java
@@ -2,12 +2,13 @@ package com.glhf.bomberball.gameobject;
 
 
 import com.glhf.bomberball.utils.Constants;
+import com.glhf.bomberball.utils.Directions;
 
 import java.util.ArrayList;
 
 public class PassiveEnemy extends Enemy {
 
-    public PassiveEnemy(String skin, int life, int initial_moves, int strength, ArrayList<Constants.moves> way) {
+    public PassiveEnemy(String skin, int life, int initial_moves, int strength, ArrayList<Directions> way) {
         super(skin, life, initial_moves, strength, way);
     }
 }
