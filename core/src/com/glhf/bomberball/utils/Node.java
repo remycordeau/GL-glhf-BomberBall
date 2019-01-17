5235b59146f04508654c7d80a537ebecd61e1453
Natan <ntalon@enssat.fr>
2019-01-17 12:11:00 +0100

    Essaye de résolution de l'explosion mémoire des ennemis

--- core/src/com/glhf/bomberball/utils/Node.java
+++ core/src/com/glhf/bomberball/utils/Node.java
@@ -10,13 +10,29 @@ public class Node {
     // Node[0] for UP, 1 for RIGHT, 2 for DOWN, 3 for LEFT
     private Node[] sons;
     private Cell matching_cell;
-
+    //NEW
+    private Node ancestor;
+    private int way_size;
+    private int son_set;
+    //
 
     public Node(ArrayList<Cell> ancestors, Cell matching_cell){
         this.ancestors = ancestors;
         this.matching_cell = matching_cell;
         this.sons = new Node[4];
     }
+
+    //NEW
+    public Node(ArrayList<Cell> ancestors, Node ancestor, Cell matching_cell) {
+        this.ancestors = ancestors;
+        this.matching_cell = matching_cell;
+        this.sons = new Node[4];
+        this.ancestor = ancestor;
+        this.way_size = -1;
+        this.son_set = 0;
+    }
+    //
+
 
     public void setSons(Node son, Directions dir){
         this.sons[dir.ordinal()] = son;
@@ -32,38 +48,36 @@ public class Node {
 
     public Node getSons(int index){return  sons[index];}
 
-    /**
-     * this method gives the direction follow from the previous cell to reach the current cell
-     * @param ancestor
-     * @return Direction
-     */
-    public Directions get_direction_from_ancestor(Cell ancestor){
-        for(Directions d : Directions.values()){
-            if(ancestor.getAdjacentCell(d) == matching_cell ){
-                return d;
-            }
-        }
-        return null;
+    //NEW
+    public int getWay_size() {
+        return way_size;
     }
 
-    /**
-     * recursive method that gives the longest way for an initial cell (the matching_cell)
-     * @return the sequence of UP, RIGHT, DOWN, LEFT for the longest way
-     */
-    public ArrayList<Directions> get_longest_way(){
-        ArrayList<Directions> current_longest_way = new ArrayList<>();
-        current_longest_way.add(get_direction_from_ancestor(ancestors.get(ancestors.size()-1)));
-        ArrayList<Directions> current_counting;
-        boolean stop_condition = sons[0]==null && sons[1]==null && sons[2]==null && sons[3]==null;
-        if(!stop_condition){
-            for(int i=0; i<4; i++){
-                current_counting = sons[i].get_longest_way();
-                if(current_counting.size() >= current_longest_way.size()){
-                    current_longest_way = current_counting;
-                }
+    private void setWay_size() {
+        int max = 0;
+        int max_size = 0;
+        int i;
+        for (i=0; i<4; i++) {
+            if (sons[i] != null && sons[i].getWay_size() > max_size) {
+                max_size = sons[i].getWay_size();
+                max = i;
             }
         }
-        return current_longest_way;
+        for (i=0; i<4; i++) {
+            if (i != max) {
+                this.setSons(null, Directions.values()[i]);
+            }
+        }
+        this.way_size = max_size + 1;
     }
+
+    public void sonIsSet() {
+        son_set++;
+        if (son_set == 4) {
+            this.setWay_size();
+            ancestor.sonIsSet();
+        }
+    }
+    //
 }
 
